2b489c375d0f53a6eeebf67238c2375e2c0fdc13
lgope <ilakshmangope@gmail.com>
2019-12-15 23:22:59 +0600

    created react app & added files for homepage 🗽🚀

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import HomePage from "./homepage.component.jsx";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
