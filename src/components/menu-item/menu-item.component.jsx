77fe276fffe0c485d6d84e0e92c9659813b4313e
lgope <ilakshmangope@gmail.com>
2019-12-16 01:01:23 +0600

    styling menu item compnent 🗽🍀

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
 
-import './homepage.styles.scss';
+import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <Directory/>
+    <Directory />
   </div>
 );
 
