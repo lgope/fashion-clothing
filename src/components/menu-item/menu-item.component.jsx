59b67c8483d23861f9f12da50bf29d23d0ad95b0
lgope <ilakshmangope@gmail.com>
2019-12-16 00:29:53 +0600

    maked component for hpage, dirctry, menuItem 🗽❄

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
