c09242fc1b59811201e6638f47d3a7e20cef23f9
lgope <ilakshmangope@gmail.com>
2019-12-20 00:22:25 +0600

    implementd router with react-router-dom 🗽🚀

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,9 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+          // key={id} title={title} imageUrl={imageUrl} size={size} history={this.props.history}
         ))}
       </div>
     );
