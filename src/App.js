6f4a6f056b8491db3dccf9109bd0600158226bc8
lgope <ilakshmangope@gmail.com>
2019-12-21 02:47:02 +0600

    maked signin component with form input component 🗽📥

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.styles.scss";
+import "./homepage.styles.css.map";
 
 const HomePage = () => (
   <div className="homepage">
