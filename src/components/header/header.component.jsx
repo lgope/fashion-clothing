c44efba59c1b3db04fe99f07db35d1e26b6bab78
lgope <ilakshmangope@gmail.com>
2019-12-21 15:12:43 +0600

    Sign In|Out with firebase google auth 🗽🛠

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/fashionC.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
-      <Logo classNam="logo" />
+      <Logo className="logo" />
     </Link>
+
     <div className="options">
       <Link className="option" to="/shop">
         SHOP
@@ -18,6 +21,16 @@ const Header = () => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
+
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
