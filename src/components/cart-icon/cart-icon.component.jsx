b116091c78f2bf24803c100bc1010b8a5a21061d
lgope <ilakshmangope@gmail.com>
2020-01-16 21:43:42 +0600

    cart dropdown, styling, redux in cart, button 🗽🌟

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-  <div className='cart-icon'>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+  <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import "./collection-item.styles.scss";
+import CustomButton from '../custom-button/custom-button.component';
+
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
+  <div className='collection-item'>
     <div
-      className="image"
+      className='image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
     />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
