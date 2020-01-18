7d534c1c7586ea1caa68426a14550a94c5ec235d
lgope <ilakshmangope@gmail.com>
2020-01-18 22:18:57 +0600

    Checkout page, Add, Remove items 🗽🛠

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
@@ -50,6 +53,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route
             path='/signin'
             render={() =>
