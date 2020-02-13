8c4fded447089380115c8a3fe5abb813fb2e0722
lgope <ilakshmangope@gmail.com>
2020-02-13 23:40:16 +0600

    redux firebase | withspinner 🚀

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
